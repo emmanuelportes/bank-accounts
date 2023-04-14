0acfd24c3c8626f750fbde7d794f9e60862a9e74
Emmanuel <mportesz.08@gmail.com>
2023-04-14 15:24:56 -0400

    added withdraw functionality

--- models/account.py
+++ models/account.py
@@ -6,31 +6,52 @@ class Account:
         self._id = self._generate_id()
         self._balance = 0.0
 
-    # method to deposit money to the account
     def deposit(self, amount: float) :
-        if (type(amount) is not float) and amount <= 0.0 :
-            raise ValueError("invalid ammount provided")
-        
-        self._balance = self._balance + amount
+        self._validate_amount(amount)     
+        self.balance += amount
                 
-    # method to withdraw money from the account
+    def withdraw(self, amount: float) :
+        self._validate_amount(amount)
+        remaining = self.balance - amount
 
-    # method to transfer to the account
-    # method to show balance
-    @property
-    def balance(self) -> float:
-        return self._balance
-    
+        if remaining > 0:
+            self.balance = remaining
+
+
     @property
     def id(self) -> str :
         return self._id
+
+    @property
+    def balance(self) -> float :
+        return self._balance
+    
+    @balance.setter
+    def balance( self, balance: float) :
+
+        if not isinstance(balance, float) :
+            raise TypeError("invalid value type provided") 
         
+        if balance <= 0 :
+            raise ValueError("invalid balance provided")
+        
+        self._balance = balance
+       
+
+    def _validate_amount(self, amount: float) :
+           
+           if not isinstance(amount, float):
+               raise TypeError("invalid value type provided")
+           
+           if amount <= 0.0 :
+               raise ValueError("invalid amount provided")
+    
     def _generate_id(self) -> str:
         return uuid.uuid4()
 
 
 acc = Account()
 
-acc.deposit(500.0)
+acc.deposit(100.0)
 print(acc.balance)
-
+print(acc.id)

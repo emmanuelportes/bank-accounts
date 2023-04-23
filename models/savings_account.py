208e042b5d6929f195b8380cce363fc7e4dc4862
Emmanuel Portes <mportesz.08@gmail.com>
2023-04-22 23:55:54 -0400

    modified withdraw and deposit functionalities

--- models/savings_account.py
+++ models/savings_account.py
@@ -5,17 +5,20 @@ class SavingsAccount(Account):
     def __init__(self, balance: float, annual_interest_rate: float):
         super().__init__(balance, annual_interest_rate)
         self._ALLOWED_BALANCE = 25.0
-        self.status = self._is_account_active()
+        self.status = self._is_active()
 
     def deposit(self, amount: float):
-        pass
+        self._validate_amount(amount)
+        if not self.status and amount > self._ALLOWED_BALANCE :
+            self.status = True
+            super().deposit(amount)
 
     def withdraw(self, amount: float):
-        pass
+        if self.status:
+            super().withdraw(amount)
 
-    def _is_account_active(self):
+    def _is_active(self):
         status : bool = True
-        
         if self.balance < self._ALLOWED_BALANCE: 
             status = False
 

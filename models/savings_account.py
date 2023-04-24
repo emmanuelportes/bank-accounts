5780a83b569331b763f1f7965fa635b0155db61e
Emmanuel Portes <mportesz.08@gmail.com>
2023-04-23 23:31:59 -0400

    change is_active functionality

--- models/savings_account.py
+++ models/savings_account.py
@@ -5,23 +5,21 @@ class SavingsAccount(Account):
     def __init__(self, balance: float, annual_interest_rate: float):
         super().__init__(balance, annual_interest_rate)
         self._ALLOWED_BALANCE = 25.0
-        self.status = self._is_active()
+        self.status = True
 
     def deposit(self, amount: float):
-        self._validate_amount(amount)
-        if not self.status and amount > self._ALLOWED_BALANCE :
+        super()._validate_amount(amount)
+        if not self._is_active() and amount > self._ALLOWED_BALANCE :
             self.status = True
             super().deposit(amount)
 
     def withdraw(self, amount: float):
-        if self.status:
+        if self._is_active():
             super().withdraw(amount)
 
     def _is_active(self):
-        status : bool = True
         if self.balance < self._ALLOWED_BALANCE: 
-            status = False
-
-        return status
+            self.status = False
+        return self.status
 
     

68fd7449317964f610f0fe6c802d28130c914ede
Emmanuel Portes <mportesz.08@gmail.com>
2023-04-24 23:21:15 -0400

    added monthly process functionality

--- models/savings_account.py
+++ models/savings_account.py
@@ -10,16 +10,26 @@ class SavingsAccount(Account):
     def deposit(self, amount: float):
         super()._validate_amount(amount)
         if not self._is_active() and amount > self._ALLOWED_BALANCE :
-            self.status = True
+            self._change_status()
             super().deposit(amount)
 
     def withdraw(self, amount: float):
         if self._is_active():
             super().withdraw(amount)
 
-    def _is_active(self):
+    def _monthly_process(self):
+        if self.number_of_withdrawals > 4:
+            self._service_charges = self.number_of_withdrawals - 4
+            super()._monthly_process()
+
+            if self._is_active() :
+                self._change_status()
+
+    def _change_status(self) :
         if self.balance < self._ALLOWED_BALANCE: 
             self.status = False
-        return self.status
+        else:
+            self.status = True
 
-    
+    def _is_active(self):
+        return self.status
